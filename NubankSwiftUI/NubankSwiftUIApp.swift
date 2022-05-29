cda87f08f75cef2874ab788be2f29825ec60ac2d
brianail <80833852+brianail@users.noreply.github.com>
2022-05-29 14:58:06 -0300

    Initial Commit

---
+++ NubankSwiftUI/NubankSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubankSwiftUIApp.swift
+//  NubankSwiftUI
+//
+//  Created by Brian Ail on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct NubankSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
